07e539f059b1957a0d1ecc3a6031fa6242546288
mindy <mindyhammock@gmail.com>
2016-09-20 15:36:09 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codeval_Longest_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "I have a dog";
+
+            int sentLeng = sentence.Length;
+            string[] sent = sentence.Split(' ');
+
+            for (int i = 0; i < sent.Length; i++)
+            {
+                Console.WriteLine(sent[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
